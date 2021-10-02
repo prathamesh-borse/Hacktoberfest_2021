e0052fc1c982f5d44fbc8b21e4e54437aaab368f
Aditya Jadhav <83344372+aditya-517@users.noreply.github.com>
2021-10-02 16:21:23 +0530

    Added LeetCode Question Link

--- leetcode/Easy_Q771.java
+++ leetcode/Easy_Q771.java
@@ -4,6 +4,8 @@ Question :
        Each character in stones is a type of stone you have.
        You want to know how many of the stones you have are also jewels.
        Letters are case sensitive, so "a" is considered a different type of stone from "A".
+       
+Leetcode Link : https://leetcode.com/problems/jewels-and-stones/
 
 Sample Testcases : 
 
